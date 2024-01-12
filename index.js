7e8d1c8a36857f2697a9e4b00e34b06a24365c48
Haris A <108242470+haristiks@users.noreply.github.com>
2024-01-12 20:40:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,25 +13,21 @@
 //   const url = getSignedUrl(s3Client, command);
 //   return url;
 // }
-
 // async function putObject(filename, contentType) {
 //   const command = new PutObjectCommand({
 //     Bucket: "bgon.ams.dev",
 //     Key: `Test/user-uploads/${filename}`,
 //     ContentType: contentType,
 //   });
-
 //   const url = await getSignedUrl(s3Client, command);
 //   return url;
 // }
-
 // async function init() {
 //   //retriving
 //   console.log(
 //     "URL for Pexels",
 //     await getObjectURL("Test/pexels-nicola-giordano-1101296.jpg")
 //   );
-
 //   //uploading
 //   console.log(
 //     "URL for Uploading",
